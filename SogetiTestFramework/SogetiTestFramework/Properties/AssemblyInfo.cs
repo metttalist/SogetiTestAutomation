62de6998fffdd46c4a2f9783e5e82e628014548d
Bertram, Jeffrey <Jeffrey.Bertram@us.sogeti.com>
2018-03-28 10:44:18 -0500

    Creating a new solution and project for Sogeti Test Automaiton framework

---
+++ SogetiTestFramework/SogetiTestFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SogetiTestFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SogetiTestFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("06cf213b-787a-4d74-97bb-067a3cc4a42a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
