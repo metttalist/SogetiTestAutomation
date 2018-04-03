7a08c027554b4c50d678df19cf8c9f45f0144e43
Macgowan <Chris.Macgowan@us.sogeti.com>
2018-04-03 15:07:50 -0500

    update basepage and basewebdriver classes

--- SogetiTestFramework/SogetiTestFramework/Page/BasePage.cs
+++ SogetiTestFramework/SogetiTestFramework/Page/BasePage.cs
@@ -3,42 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SogetiTestFramework.Utility;
 
-/**
- * The BasePage class will implement the functionality related to the page object. 
- * Page classes that support specific webpages will inherit from this class. 
- * 
- * @Author Chris Macgowan
- * @Created 
- * 
- * @copyright All rights reserved by Capgemini. Proprietary and Confidential information of 
- * Capgemini. Disclosure, Use or Reproduction without the written authorization of 
- * Capgemini is prohibited.
- * 
- * This is a test of the check-in process 
- * Updating comment
- */
-
-
-
-
-
-/// 
-/// <copyright file=$safeitemrootname$ company="My Company Name">
-/// Copyright(c) 2012 All Rights Reserved
-/// </copyright>
-   <author>Leniel Macaferi</author>
-   <date>$time$</date>
-   <summary>Class representing a Sample entity</summary>*/
-
-/// <summary>
-/// The GetZero method.
-/// </summary>
-/// <example> 
 
 namespace SogetiTestFramework.Page
 {
+
+    /// <summary>
+    /// The BasePage class will implement the functionality related to the page 
+    /// object. Page classes that support specific webpages will inherit from this class.
+    /// </summary>
+    /// <author>Chris Macgowan</author>
+    /// <date>30 Mar 2018</date>
+    /// <copyright>
+    /// All rights reserved by Capgemini. Copyright © 2018 Capgemini. Proprietary and 
+    /// Confidential information of Capgemini. Disclosure, Use or Reproduction 
+    /// without the written authorization of Capgemini is prohibited.
+    /// </copyright>
     class BasePage
     {
+
+        // The following objects are used to support the BasePage object
+        // The commented lines are placeholders for future development
+        
+        // protected PropertiesManager properties;
+
+        // protected TestConfiguration config;
+
+        protected BaseWebDriver webDriver;
+        
+        // private final static Log logger = new Log(AbstractPage.class);
+
+
     }
 }
--- SogetiTestFramework/SogetiTestFramework/Utility/BaseWebDriver.cs
+++ SogetiTestFramework/SogetiTestFramework/Utility/BaseWebDriver.cs
@@ -4,9 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
 namespace SogetiTestFramework.Utility
 {
+
+    /// <summary>
+    /// The BaseWebDriver class will implement the functionality related to the Selenuim  
+    /// WebDriver object. BaseWebDriver is exposed to the test Page classes through the 
+    /// BasePage Class.
+    /// </summary>
+    /// <author>Chris Macgowan</author>
+    /// <date>31 Mar 2018</date>
+    /// <copyright>
+    /// All rights reserved by Capgemini. Copyright © 2018 Capgemini. Proprietary and 
+    /// Confidential information of Capgemini. Disclosure, Use or Reproduction 
+    /// without the written authorization of Capgemini is prohibited.
+    /// </copyright>
+    /// <remarks>
+    /// The BaseWebDriver object will only support FirefoxDriver at this time. We will 
+    /// implement Chrome, IE and other drivers when we implement the PropertiesManager 
+    /// and TestConfiguration supporting classes. 
+    /// </remarks>
     class BaseWebDriver
     {
+        
+        // The following objects are used to support the BaseWebDriver object
+        // The commented lines are placeholders for future development
+
+        protected static FirefoxDriver driver;
+
+        // protected PropertiesManager properties;
+
+        // protected TestConfiguration config;
+        
+        // private final static Log logger = new Log(AbstractPage.class);
+
+
+        /// <summary>
+        /// Default constructor. We will create the driver object. 
+        /// </summary>
+        public void BasePage()
+        {
+            driver = new FirefoxDriver();
+        }
+
+        /// <summary>
+        /// Return the FirefoxDriver driver object. 
+        /// </summary>
+        /// <returns> Return the FirefoxDriver driver object.</returns>
+        public FirefoxDriver getDriver()
+        {
+            return driver;
+        }
     }
 }
