ba4a14ae276722c60e63cd33555bba50404b191a
Macgowan <Chris.Macgowan@us.sogeti.com>
2018-04-03 10:26:07 -0500

    update some source doc also added the BaseWedDriver class

--- SogetiTestFramework/SogetiTestFramework/Page/BasePage.cs
+++ SogetiTestFramework/SogetiTestFramework/Page/BasePage.cs
@@ -5,10 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 /**
- * We are adding some documentation here 
+ * The BasePage class will implement the functionality related to the page object. 
+ * Page classes that support specific webpages will inherit from this class. 
+ * 
+ * @Author Chris Macgowan
+ * @Created 
+ * 
+ * @copyright All rights reserved by Capgemini. Proprietary and Confidential information of 
+ * Capgemini. Disclosure, Use or Reproduction without the written authorization of 
+ * Capgemini is prohibited.
+ * 
  * This is a test of the check-in process 
  * Updating comment
  */
+
+
+
+
+
+/// 
+/// <copyright file=$safeitemrootname$ company="My Company Name">
+/// Copyright(c) 2012 All Rights Reserved
+/// </copyright>
+   <author>Leniel Macaferi</author>
+   <date>$time$</date>
+   <summary>Class representing a Sample entity</summary>*/
+
+/// <summary>
+/// The GetZero method.
+/// </summary>
+/// <example> 
 
 namespace SogetiTestFramework.Page
 {
