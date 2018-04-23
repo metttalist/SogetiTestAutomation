9eaa53aac191653e5d72920f530c2fa6e95f4f3b
Bertram, Jeffrey <Jeffrey.Bertram@us.sogeti.com>
2018-04-23 11:08:31 -0500

    Remove unused references

--- SogetiTestFramework/SampleAPITestProject/StepDefinition/ApiTestStepDef.cs
+++ SogetiTestFramework/SampleAPITestProject/StepDefinition/ApiTestStepDef.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using RestSharp;
-using SogetiTestFramework.Helper;
+﻿using SogetiTestFramework.Helper;
 using SogetiTestFramework.Rest;
 using TechTalk.SpecFlow;
 
